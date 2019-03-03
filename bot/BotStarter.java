154b1a30b589ad83a33ed72142b9e5a0e8211fff
yafi38 <yafi2475@gmail.com>
2019-03-03 14:12:43 +0600

    fixed gitignore

---
+++ bot/BotStarter.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2016 riddles.io (developers@riddles.io)
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ *     For the full copyright and license information, please view the LICENSE
+ *     file that was distributed with this source code.
+ */
+
+package bot;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import board.Board;
+import logic.Logic;
+
+/**
+ * bot.BotStarter
+ * 
+ * Magic happens here. You should edit this file, or more specifically
+ * the doMove() method to make your bot do more than random moves.
+ * 
+ * @author Jim van Eeden <jim@riddles.io>, Joost de Meij <joost@riddles.io>
+ */
+
+public class BotStarter {
+
+     /**
+      * Makes a turn. Edit this method to make your bot smarter.
+      * @return The column where the turn was made.
+      */
+     public int doMove(BotState state) {
+         Board board = state.getBoard();
+         //int move = new Random().nextInt(board.getWidth());
+
+         ArrayList<Integer> moves = state.getValidMoves();
+
+         int move = new Random().nextInt(moves.size());
+         move = moves.get(move);
+
+         if(Logic.isWinning(board, 4, board.getMyId())) {
+             System.err.println("I am winning");
+         }
+
+         return move;
+     }
+     
+ 	public static void main(String[] args) {
+ 		BotParser parser = new BotParser(new BotStarter());
+ 		parser.run();
+ 	}
+ }
---
+++ bot/BotState.java
@@ -0,0 +1,103 @@
+package bot;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import board.Board;
+import player.Player;
+
+/**
+ * bot.BotState - Created on 11-5-17
+ *
+ * Keeps track of the complete game state, such as the board
+ * and game settings
+ *
+ * @author Jim van Eeden - jim@riddles.io
+ */
+public class BotState {
+
+    private int MAX_TIMEBANK;
+    private int TIME_PER_MOVE;
+    private int MAX_ROUNDS;
+
+    private int roundNumber;
+    private int timebank;
+    private String myName;
+    private HashMap<String, Player> players;
+
+    private Board board;
+
+    BotState() {
+        this.board = new Board();
+        this.players = new HashMap<>();
+    }
+
+    public void setTimebank(int value) {
+        this.timebank = value;
+    }
+
+    public void setMaxTimebank(int value) {
+        this.MAX_TIMEBANK = value;
+    }
+
+    public void setTimePerMove(int value) {
+        this.TIME_PER_MOVE = value;
+    }
+
+    public void setMyName(String myName) {
+        this.myName = myName;
+    }
+
+    public void setMaxRounds(int value) {
+        this.MAX_ROUNDS = value;
+    }
+
+    public void setRoundNumber(int roundNumber) {
+        this.roundNumber = roundNumber;
+    }
+
+    public int getTimebank() {
+        return this.timebank;
+    }
+
+    public int getRoundNumber() {
+        return this.roundNumber;
+    }
+
+    public HashMap<String, Player> getPlayers() {
+        return this.players;
+    }
+
+    public Board getBoard() {
+        return this.board;
+    }
+
+    public String getMyName() {
+        return this.myName;
+    }
+
+    public int getMaxTimebank() {
+        return this.MAX_TIMEBANK;
+    }
+
+    public int getTimePerMove() {
+        return this.TIME_PER_MOVE;
+    }
+
+    public int getMaxRound() {
+        return this.MAX_ROUNDS;
+    }
+
+    public ArrayList<Integer> getValidMoves() {
+        ArrayList<Integer> moves = new ArrayList<>();
+
+        for(int i=0; i<7; i++)
+        {
+            Point p = new Point(0, i);
+            if(board.getFieldAt(p).equals(Board.EMPTY_FIELD))
+                moves.add(i);
+        }
+        return moves;
+    }
+}
